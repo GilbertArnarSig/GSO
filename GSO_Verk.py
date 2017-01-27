c3d45af3c1d7029b73d2fc1984f52dcdbeed91d6
GilbertArnarSig <gilbertsi255@nemi.tskoli.is>
2017-01-27 09:54:10 +0000

    Buinn að commenta verkefni

--- GSO_Verk.py
+++ GSO_Verk.py
@@ -2,14 +2,14 @@
 # 27.01.17
 # Git verkefni fyrir GSÖ
 
-f = open("GSO.txt","w+")
-f.write("Halló \r\n")
-f.close()
-f = open("GSO.txt","a+")
-for x in range(3):
-    f.write("Þetta er lína %d\r\n" % (x+1))
-f.close()
-f = open("GSO.txt","r+")
-skilabod = f.read()
-print(skilabod)
-f.close()
+f = open("GSO.txt","w+") # notað til að búa til texta skrá
+f.write("Halló \r\n") # Notað til að skrifa í texta skrána
+f.close() # Lokar texta skráni
+f = open("GSO.txt","a+") # Opnar texta skrána til að bæta inní hana
+for x in range(3): # For lykkja til að prenta þrjár línur
+    f.write("Þetta er lína %d\r\n" % (x+1)) # Skrifar inní texta skrána
+f.close() # lokar texta skráni
+f = open("GSO.txt","r+") # opnar texta skrána til að lesa úr henni
+skilabod = f.read() # breyta til þess að prenta út textan
+print(skilabod) # byrtir innihald skránar
+f.close() # Lokar skráni
